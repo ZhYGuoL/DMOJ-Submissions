6f8e553e636434832a4e70b3ff21ad8baab9589c
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-02-04 11:25:22 -0800

    yes

--- Done/AAC/AAC 5 P2.py
+++ Done/AAC/AAC 5 P2.py
@@ -1,40 +1,54 @@
 from math import sqrt
+from math import gcd
 
-allNumbersCombined = 0
+numbersArranged = ''
 
 N = int(input())
 
-print('? 1 2')
+if N == 3:
+    print('? 1 2')
+    zeroxOne = int(input())
 
-zeroxOne = int(input())
+    print('? 1 3')
+    zeroxTwo = int(input())
 
-print('? 1 3')
+    num1 = int(gcd(zeroxOne, zeroxTwo))
+    
+    numbersArranged = str(num1) + ' ' + str(int(zeroxOne/num1)) + ' ' + str(int(zeroxTwo/num1))
+    
+    a = num1 + zeroxOne/num1
+    
+    print('! '+ numbersArranged)
+    
+else:
 
-zeroxTwo = int(input())
-
-print('? 2 3')
-
-onexTwo = int(input())
-
-num1 = int(sqrt(zeroxOne*zeroxTwo/onexTwo))
-num2 = int(zeroxOne/num1)
-num3 = int(onexTwo/(zeroxOne/num1))
-allNumbersCombined += num1 + num2 + num3
-prevNum = num3
-numbersArranged = str(num1) + ' ' + str(num2) + ' ' + str(num3) + ' '
-
-
-for curNum in range(4, N):
-    print('? ' + str(curNum-1) + ' ' + str(curNum))
-
-    prevNum = int(int(input())/prevNum)
-
-    numbersArranged += str(prevNum) + ' '
-
-    allNumbersCombined += prevNum
-
-a = 0
-for i in range(N+1):
-    a += i
-
-print('! '+ numbersArranged + str(int(a-allNumbersCombined)))
+    print('? 1 2')
+    zeroxOne = int(input())
+    
+    print('? 1 3')
+    zeroxTwo = int(input())
+    
+    print('? 2 3')
+    onexTwo = int(input())
+    
+    num1 = int(sqrt(zeroxOne*zeroxTwo/onexTwo))
+    num2 = int(zeroxOne/num1)
+    num3 = int(onexTwo/(zeroxOne/num1))
+    allNumbersCombined = num1 + num2 + num3
+    prevNum = num3
+    numbersArranged = str(num1) + ' ' + str(num2) + ' ' + str(num3) + ' '
+    
+    for curNum in range(4, N):
+        print('? ' + str(curNum-1) + ' ' + str(curNum))
+    
+        prevNum = int(int(input())/prevNum)
+    
+        numbersArranged += str(prevNum) + ' '
+    
+        allNumbersCombined += prevNum
+    
+    a = 0
+    for i in range(N+1):
+        a += i
+    
+    print('! '+ numbersArranged + str(int(a-allNumbersCombined)))
