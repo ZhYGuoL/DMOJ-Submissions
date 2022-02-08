a12c48d71e8055715afa3d00e81ae5e5a948d05c
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-02-08 00:18:41 -0800

    WA the last testcase of every batch

--- Priority/CCC Mock '22 J3.py
+++ Priority/CCC Mock '22 J3.py
@@ -1,5 +1,6 @@
 chars = {}
 
+useless = input()
 for wordNum in range(2):
     for char in input():
         if char in chars:
@@ -13,22 +14,18 @@ for wordNum in range(2):
 maxNum = 0
 minNum = 6969696969
 
-
-
 total = 0
 
-print(chars)
 for char in chars:
-    if chars[char][0] >= maxNum:
-        maxNum = chars[char][0]
-    elif chars[char][0] <= minNum and chars[char][1] >= 0:
-        minNum = chars[char][0]
-    total += chars[char][0]*chars[char][1]
+    num1, num2 = chars[char]
+    if num1 > maxNum:
+        maxNum = num1
+    if num1 < minNum and num2 >= 0:
+        minNum = num1
+    total += num1*num2
 
-print(pair1, pair2)
 
-# print(total+(pair1[0]*(pair1[1]+1)-pair1[0]*pair[1]))
-print(total+pair1[0]-pair2[0])
+print(total+maxNum-minNum)
 
 
 # crosses = 0
