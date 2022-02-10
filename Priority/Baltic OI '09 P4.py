724d812db1ab7557799f0fe8e01c3a46a107d5b5
Zhiyuan Guo <zhiyuang2006@gmail.com>
2022-02-10 08:58:37 -0800

    Finding biggest area of a rectangle from 4 points

--- Priority/Baltic OI '09 P4.py
+++ Priority/Baltic OI '09 P4.py
@@ -10,13 +10,14 @@ maxArea = 0
 
 for a, b, c, d in combinations(coords, 4):
     center = (a[0]+b[0]+c[0]+d[0])/4, (a[1]+b[1]+c[1]+d[1])/4
-    print(a)
-    aDist = sqrt((max(a[0], center[0])-min(center[0], a[0]))**2+(max(a[1], center[1]-min(a[1], center[1])))**2)
+    print(a, b, c, d)
+    aDist = sqrt((max(a[0], center[0])-min(center[0], a[0]))**2+(max(a[1], center[1])-min(a[1], center[1])))**2)
     print(center)   
     print('distance:', aDist)
     if aDist == sqrt((max(b[0], center[0])-min(center[0], b[0]))**2+(max(b[1], center[1])-min(b[1], center[1]))**2) and aDist == sqrt((max(c[0], center[0])-min(center[0], c[0]))**2+(max(c[1], center[1])-min(c[1], center[1]))**2) and aDist == sqrt((max(d[0], center[0])-min(center[0], d[0]))**2+(max(d[1], center[1])-min(d[1], center[1]))**2):
         maxArea = int(max(maxArea, ((a[0]*b[1]+b[0]*c[1]+c[0]*d[1]+d[0]*a[1])-(a[1]*b[0]+b[1]*c[0]+c[1]*d[0]+d[1]*a[0]))/2))
     print(center[0])
+    print('\n')
 
 print(maxArea) 
 
