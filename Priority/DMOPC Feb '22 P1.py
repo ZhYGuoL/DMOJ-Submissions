f21050d32ca61b9c0ac9db89d9f3652ac9cb50c8
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-02-14 00:34:33 -0800

    DMOPC

--- Priority/DMOPC Feb '22 P1.py
+++ Priority/DMOPC Feb '22 P1.py
@@ -1,16 +1,3 @@
-# useless = int(input())
-# number = [int(x) for x in input()]
-
-# for i in range(useless-1):
-#     if number[i] >= number[i+1]:
-#         continue
-#     temp = number[i]
-#     number[i] = number[i+1]
-#     number[i+1] = temp
-#     break
-# for char in number:
-#     print(char, end='')
-
 length = int(input())
 number = input()
 for idx in range(length-1):
