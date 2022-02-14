91a11f17954d914c489d667439e8940d8229f897
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-02-14 00:29:55 -0800

    DMOPC

--- Priority/DMOPC Feb '22 P2.py
+++ Priority/DMOPC Feb '22 P2.py
@@ -1,17 +1,20 @@
-n, k = input().split(' ')
-points = input().split('1')
-points.sort(key = len)
-counter = 0
-for i in range(int(k)):
-    counter += len(points[-(1+i)])
+# n, k = input().split(' ')
+# points = input().split('1')
+# points.sort(key = len)
+# counter = 0
+# for i in range(int(k)):
+#     counter += len(points[-(1+i)])
 
-print(counter)
+# print(counter)
 
-n, k = input().split(' ')
+n, k = list(map(int, input().split(' ')))
 points = input().split('1')
 points.sort(key = len)
 L = 0
-for i in range(int(k)):
-    L += len(points[-(1+i)])
-
+if k >= len(points):
+    for i in points:
+        L += len(i)
+else:
+    for i in range(k):
+        L += len(points[-(1+i)])
 print(L)
