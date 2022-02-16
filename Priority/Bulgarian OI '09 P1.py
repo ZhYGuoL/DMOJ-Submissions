9c52ce3d74532064a0fadf65fdc5e5d6a1617a9d
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-02-16 08:18:49 -0800

    Convex Hull

--- Priority/Bulgarian OI '09 P1.py
+++ Priority/Bulgarian OI '09 P1.py
@@ -1,1 +1,18 @@
-https://dmoj.ca/problem/bgoi09p1
+numCoords, radius = [int(x) for x in input().split()]
+
+coords = []
+
+
+# def angle
+
+
+
+for _ in range(numCoords):
+    coords.append(input().split())
+
+x1, y1 = min(coords, key=lambda x: x[1])
+
+# sorted(coords, key = lambda x: x[1])
+
+
+    
