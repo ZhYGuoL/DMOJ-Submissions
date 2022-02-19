be3e2bfda0b3f3a305ba02f40e64ab1911bdc7ce
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-02-19 08:40:37 -0800

    Minesweeper - Convex Hull

--- Priority/Bulgarian OI '09 P1.py
+++ Priority/Bulgarian OI '09 P1.py
@@ -1,18 +1,28 @@
+from math import atan, degrees
+
+def angle(coord1, coord2):
+    angle = degrees(atan2(abs(coord1[0]-coord2[0]), abs(coord1[1]-coord2[1])))
+    
+    if angle < 0:
+        return True
+    elif angle > 0:
+        return False
+    else:
+        return True
+
 numCoords, radius = [int(x) for x in input().split()]
 
 coords = []
 
-
-# def angle
-
-
-
 for _ in range(numCoords):
-    coords.append(input().split())
+    coords.append([int(x) for x in input().split()])
 
-x1, y1 = min(coords, key=lambda x: x[1])
+sorted(coords, key = lambda x: x[0])
 
-# sorted(coords, key = lambda x: x[1])
+print(coords)
 
+top = [coords[0], coords[1]]
+bottom = []
 
-    
+for coord in coords:
+    while angle(coords[-1], coords[-2])
