726127d5ba22e86845fa1fbb99006868bbca9d10
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-03-01 22:04:13 -0800

    AAC

--- Done/AAC/AAC 5 P1.py
+++ Done/AAC/AAC 5 P1.py
@@ -1,13 +1,12 @@
-from math import floor
-
 batches = int(input())
 even = 0
 odd = 0
 
-for cookiesInBatch in input().split(' '):
-    if int(cookiesInBatch) % 2 == 0:
+
+for cookies in input().split():
+    if int(cookies) % 2 == 0:
         even += 1
     else:
         odd += 1
 
-print(floor(even/2)+floor(odd/2))
+print(odd//2+even//2)
