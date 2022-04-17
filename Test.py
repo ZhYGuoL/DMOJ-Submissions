0f0479b0269c061f8c944b50c595dbbb25386b23
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-04-17 12:59:26 -0700

    Testing

---
+++ Test.py
@@ -0,0 +1,21 @@
+def backwards(string):
+    backwards = ""
+    for i in range(len(string)):
+        backwards += string[-(i + 1)]
+    return backwards
+
+
+def palindromecreator(e):
+    palindrome_count = 1
+    for i in range(len(e)):
+        forwards = e[i:]
+        back = forwards[::-1]
+        for t in range(len(forwards)):
+            if (forwards[t:] in back) or (back[t:] in forwards):
+                palindrome_count = max(palindrome_count, len(forwards) - t)
+    return palindrome_count
+
+
+wordgenerator = input().lower()
+print(palindromecreator(wordgenerator))
+
