25f83a7354f0dd39aa761a6d476511603bd83189
Zhiyuan Guo <zhiyuang2007@gmail.com>
2022-04-17 12:59:17 -0700

    CCC '18 J3

--- Done/CCC/CCC '18 J3.py
+++ Done/CCC/CCC '18 J3.py
@@ -7,3 +7,18 @@ print(b, 0, c, c+d, c+d+e)
 print(c+b, c, 0, d, d+e)
 print(d+c+b, d+c, d, 0, e)
 print(e+d+c+b, c+e+d, d+e, e, 0)
+
+# different solution vvv
+
+numbers = [int(x) for x in input("enter space-separated numbers").split()]
+
+firstRow = [0]
+for i in range(len(numbers)):
+    firstRow.append(firstRow[i] + numbers[i])
+
+for i in range(len(numbers) + 1):
+    currentRow = []
+    for j in range(len(numbers) + 1):
+        distance = firstRow[j] - firstRow[i]
+        currentRow.append(abs(distance))
+    print(*currentRow)
